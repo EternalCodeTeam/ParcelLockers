4338ff13f01122510f43c87c8c05397b36e6b8a4
Jakubk15 <77227023+Jakubk15@users.noreply.github.com>
2023-02-03 14:02:59 +0100

    Update build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,6 +2,7 @@ import com.github.jengelman.gradle.plugins.shadow.tasks.ShadowJar
 
 plugins {
     `java-library`
+    application
     id("net.minecrell.plugin-yml.bukkit") version "0.5.2"
     id("xyz.jpenilla.run-paper") version "2.0.1"
     id("com.github.johnrengelman.shadow") version "7.1.2"
@@ -22,20 +23,29 @@ repositories {
 }
 
 dependencies {
+    // spigot-api
     compileOnly("org.spigotmc:spigot-api:1.19.3-R0.1-SNAPSHOT")
+    // foundation
     implementation("com.github.kangarko:Foundation:6.2.8") {
         exclude(group = "org.mineacademy.plugin", module = "*")
         exclude(group = "org.spigotmc", module = "spigot-api")
         exclude(group = "org.bukkit")
         exclude(group = "org.projectlombok")
     }
+    // expressible
     implementation("org.panda-lang:expressible:1.3.1")
+    // lombok
     compileOnly("org.projectlombok:lombok:1.18.26")
-    compileOnly("org.jetbrains:annotations:24.0.0")
     annotationProcessor("org.projectlombok:lombok:1.18.26")
+    // jetbrains annotations
+    compileOnly("org.jetbrains:annotations:24.0.0")
+    // paperlib
     implementation("io.papermc:paperlib:1.0.8")
+    // panda-utilities
     implementation("org.panda-lang:panda-utilities:0.5.2-alpha")
+    // errorprone
     errorprone("com.google.errorprone:error_prone_core:2.18.0")
+    // java discord api
     implementation("net.dv8tion:JDA:5.0.0-beta.3") {
         exclude(module = "opus-java")
     }
