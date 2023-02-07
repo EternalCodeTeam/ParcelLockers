89d45e3765462e9087e710adc38a454def3ee12f
Jakubk15 <77227023+Jakubk15@users.noreply.github.com>
2023-02-07 20:26:25 +0100

    ✨ Initial commit

---
+++ src/main/java/com/eternalcode/parcellockers/ParcelLockers.java
@@ -0,0 +1,14 @@
+package com.eternalcode.parcellockers;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class ParcelLockers extends JavaPlugin {
+
+	@Override
+	public void onEnable() {
+	}
+
+	@Override
+	public void onDisable() {
+	}
+}
