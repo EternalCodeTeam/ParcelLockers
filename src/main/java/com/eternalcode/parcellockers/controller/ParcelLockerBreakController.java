0314d753eea31c6d7ec9b4b97b6b15f74e41b649
Jakubk15 <77227023+Jakubk15@users.noreply.github.com>
2023-09-12 15:46:06 +0200

    Update src/main/java/com/eternalcode/parcellockers/controller/ParcelLockerBreakController.java
    Co-authored-by: DMK <81445555+imDMK@users.noreply.github.com>

--- src/main/java/com/eternalcode/parcellockers/controller/ParcelLockerBreakController.java
+++ src/main/java/com/eternalcode/parcellockers/controller/ParcelLockerBreakController.java
@@ -38,7 +38,7 @@ public class ParcelLockerBreakController implements Listener {
         Position position = PositionAdapter.convert(location);
         Player player = event.getPlayer();
         if (this.parcelLockerDatabaseService.positionCache().containsKey(position)) {
-            if (!player.isOp() && !player.hasPermission("parcellockers.admin.break")) {
+            if (!player.hasPermission("parcellockers.admin.break")) {
                 event.setCancelled(true);
                 this.announcer.sendMessage(player, this.messages.cannotBreakParcelLocker);
                 return;
