cd0e5b9e9a753d0b609ca784389398188109fc15
Jakubk15 <77227023+Jakubk15@users.noreply.github.com>
2023-09-14 20:10:06 +0200

    Update src/main/java/com/eternalcode/parcellockers/notification/NotificationAnnouncer.java
    Co-authored-by: DMK <81445555+imDMK@users.noreply.github.com>

--- src/main/java/com/eternalcode/parcellockers/notification/NotificationAnnouncer.java
+++ src/main/java/com/eternalcode/parcellockers/notification/NotificationAnnouncer.java
@@ -48,7 +48,10 @@ public class NotificationAnnouncer {
     }
 
     public void broadcast(String text) {
-        this.audienceProvider.all().sendMessage(this.miniMessage.deserialize(text));
+        Audience audience = this.audienceProvider.all();
+        Component message = this.miniMessage.deserialize(text);
+        
+        audience.sendMessage(message);
     }
 
     private Audience audience(CommandSender sender) {
