7691d2169743dc0cf419bb5fbd1399ac11041914
Jakubk15 <77227023+Jakubk15@users.noreply.github.com>
2023-10-15 10:07:07 +0200

    Fix build

--- src/main/java/com/eternalcode/parcellockers/parcel/ParcelManager.java
+++ src/main/java/com/eternalcode/parcellockers/parcel/ParcelManager.java
@@ -1,26 +1,20 @@
 package com.eternalcode.parcellockers.parcel;
 
 import com.eternalcode.parcellockers.configuration.implementation.PluginConfiguration;
-import com.eternalcode.parcellockers.locker.repository.LockerRepository;
 import com.eternalcode.parcellockers.notification.NotificationAnnouncer;
 import com.eternalcode.parcellockers.parcel.repository.ParcelRepository;
 import org.bukkit.command.CommandSender;
-import org.bukkit.plugin.Plugin;
 
 public class ParcelManager {
 
-    private final Plugin plugin;
     private final PluginConfiguration config;
     private final NotificationAnnouncer announcer;
     private final ParcelRepository databaseService;
-    private final LockerRepository lockerRepository;
 
-    public ParcelManager(Plugin plugin, PluginConfiguration config, NotificationAnnouncer announcer, ParcelRepository databaseService, LockerRepository lockerRepository) {
-        this.plugin = plugin;
+    public ParcelManager(PluginConfiguration config, NotificationAnnouncer announcer, ParcelRepository databaseService) {
         this.config = config;
         this.announcer = announcer;
         this.databaseService = databaseService;
-        this.lockerRepository = lockerRepository;
     }
 
     public void createParcel(CommandSender sender, Parcel parcel) {
