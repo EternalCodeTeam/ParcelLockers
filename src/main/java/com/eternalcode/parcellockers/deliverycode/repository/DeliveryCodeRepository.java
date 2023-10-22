c9eac0b09b98e3e8668ed9627c6e8cfbdfbb5ac8
Jakubk15 <77227023+Jakubk15@users.noreply.github.com>
2023-10-22 22:06:15 +0200

    Apply PR suggestions

--- src/main/java/com/eternalcode/parcellockers/deliverycode/repository/DeliveryCodeRepository.java
+++ src/main/java/com/eternalcode/parcellockers/deliverycode/repository/DeliveryCodeRepository.java
@@ -2,6 +2,7 @@ package com.eternalcode.parcellockers.deliverycode.repository;
 
 import com.eternalcode.parcellockers.deliverycode.DeliveryCode;
 
+import java.util.Optional;
 import java.util.UUID;
 import java.util.concurrent.CompletableFuture;
 
@@ -9,7 +10,7 @@ public interface DeliveryCodeRepository {
 
     CompletableFuture<Void> save(DeliveryCode deliveryCode);
 
-    CompletableFuture<DeliveryCode> findByUUID(UUID parcelUUID);
+    CompletableFuture<Optional<DeliveryCode>> findByUUID(UUID parcelUUID);
 
     CompletableFuture<Void> remove(DeliveryCode deliveryCode);
 
