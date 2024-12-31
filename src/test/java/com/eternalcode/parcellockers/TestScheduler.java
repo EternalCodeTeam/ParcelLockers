8b2634053dbe852c4e0cd4f369f4f78d05221c8c
Jakubk15 <77227023+Jakubk15@users.noreply.github.com>
2024-12-31 16:56:55 +0100

    Add TestScheduler

---
+++ src/test/java/com/eternalcode/parcellockers/TestScheduler.java
@@ -0,0 +1,86 @@
+package com.eternalcode.parcellockers;
+
+import com.eternalcode.commons.scheduler.Scheduler;
+import com.eternalcode.commons.scheduler.Task;
+
+import java.time.Duration;
+import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.Executors;
+import java.util.concurrent.Future;
+import java.util.concurrent.ScheduledExecutorService;
+import java.util.concurrent.ScheduledFuture;
+import java.util.concurrent.TimeUnit;
+import java.util.function.Supplier;
+
+public class TestScheduler implements Scheduler {
+    private final ScheduledExecutorService executorService = Executors.newScheduledThreadPool(8);
+
+    @Override
+    public Task run(Runnable runnable) {
+        Future<?> future = executorService.submit(runnable);
+        return new TestTask(future, false);
+    }
+
+    @Override
+    public Task runAsync(Runnable runnable) {
+        Future<?> future = CompletableFuture.runAsync(runnable, executorService);
+        return new TestTask(future, false);
+    }
+
+    @Override
+    public Task runLater(Runnable runnable, Duration duration) {
+        ScheduledFuture<?> future = executorService.schedule(runnable, duration.toMillis(), TimeUnit.MILLISECONDS);
+        return new TestTask(future, false);
+    }
+
+    @Override
+    public Task runLaterAsync(Runnable runnable, Duration duration) {
+        ScheduledFuture<?> future = executorService.schedule(() -> CompletableFuture.runAsync(runnable, executorService), duration.toMillis(), TimeUnit.MILLISECONDS);
+        return new TestTask(future, false);
+    }
+
+    @Override
+    public Task timer(Runnable runnable, Duration initialDelay, Duration period) {
+        ScheduledFuture<?> future = executorService.scheduleAtFixedRate(runnable, initialDelay.toMillis(), period.toMillis(), TimeUnit.MILLISECONDS);
+        return new TestTask(future, true);
+    }
+
+    @Override
+    public Task timerAsync(Runnable runnable, Duration initialDelay, Duration period) {
+        ScheduledFuture<?> future = executorService.scheduleAtFixedRate(() -> CompletableFuture.runAsync(runnable, executorService), initialDelay.toMillis(), period.toMillis(), TimeUnit.MILLISECONDS);
+        return new TestTask(future, true);
+    }
+
+    @Override
+    public <T> CompletableFuture<T> complete(Supplier<T> supplier) {
+        return CompletableFuture.supplyAsync(supplier, executorService);
+    }
+
+    @Override
+    public <T> CompletableFuture<T> completeAsync(Supplier<T> supplier) {
+        return CompletableFuture.supplyAsync(supplier, executorService);
+    }
+
+    private record TestTask(Future<?> future, boolean isRepeating) implements Task {
+
+        @Override
+        public void cancel() {
+            future.cancel(false);
+        }
+
+        @Override
+        public boolean isCanceled() {
+            return future.isCancelled();
+        }
+
+        @Override
+        public boolean isAsync() {
+            return future instanceof CompletableFuture;
+        }
+
+        @Override
+        public boolean isRunning() {
+            return !future.isDone();
+        }
+    }
+}
