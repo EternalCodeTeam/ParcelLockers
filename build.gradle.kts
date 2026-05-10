b88924ac39f38bf53f270da2b0e5cbbfcc0d0259
Jakubk15 <77227023+Jakubk15@users.noreply.github.com>
2026-05-10 19:02:28 +0200

    remove lombok from deps

--- build.gradle.kts
+++ build.gradle.kts
@@ -49,10 +49,6 @@ dependencies {
     paperLibrary("com.h2database:h2:2.4.240")
     paperLibrary("org.postgresql:postgresql:42.7.10")
 
-    // lombok
-    compileOnly("org.projectlombok:lombok:1.18.44")
-    annotationProcessor("org.projectlombok:lombok:1.18.44")
-
     // jetbrains annotations
     compileOnly("org.jetbrains:annotations:26.1.0")
 
