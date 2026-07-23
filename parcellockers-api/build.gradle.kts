57c52e1c7232b24d118efd737aa942f80372da32
Jakubk15 <77227023+Jakubk15@users.noreply.github.com>
2026-07-23 18:31:28 +0200

    build: publish ParcelLockers API artifact

--- parcellockers-api/build.gradle.kts
+++ parcellockers-api/build.gradle.kts
@@ -1,5 +1,6 @@
 plugins {
     `java-library`
+    `maven-publish`
 }
 
 dependencies {
@@ -10,4 +11,30 @@ dependencies {
     testRuntimeOnly("io.papermc.paper:paper-api:${Versions.PAPER_API}")
     testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine:${Versions.JUNIT}")
     testRuntimeOnly("org.junit.platform:junit-platform-launcher")
+}
+
+java {
+    withSourcesJar()
+    withJavadocJar()
+}
+
+publishing {
+    publications {
+        create<MavenPublication>("maven") {
+            artifactId = "parcellockers-api"
+            from(components["java"])
+        }
+    }
+
+    repositories {
+        mavenLocal()
+        maven {
+            name = "eternalcodeReleases"
+            url = uri("https://repo.eternalcode.pl/releases")
+            credentials {
+                username = System.getenv("ETERNAL_CODE_MAVEN_USERNAME")
+                password = System.getenv("ETERNAL_CODE_MAVEN_PASSWORD")
+            }
+        }
+    }
 }
